b429698d941c3e64204585df62359ceba4c8b47f
Maksim Zarubin <89940028777@ya.ru>
2025-02-10 18:12:58 +0700

    update__surroundings_text

--- components/surround.js
+++ components/surround.js
@@ -25,28 +25,17 @@ const sendSurrounding =  async (bot, chatId, data) => {
     reply_markup: {
       keyboard: [
         [menuButtons.back],
+        [menuButtons.to_main_menu],
         ...Object.values(data.items).map(item => [item.title])
       ]
     }
   })
-
-
-  // try {
-  //   await bot.sendPhoto(chatId, surroundingsDescriptions[data].image, {
-  //     caption: surroundingsDescriptions[data].description
-  //   })
-  //   await bot.sendLocation(chatId, surroundingsDescriptions[data].latitude, surroundingsDescriptions[data].longtitude)
-  // }
-  // catch (error) { 
-  //   console.log(error)
-  // }
 }
 
 const sendExactSurrounding = async (bot, chatId, data) => {
   try {
-    await bot.sendPhoto(chatId, data.image, {
-      caption: data.description
-    })
+    await bot.sendMediaGroup(chatId, data.images)
+    await bot.sendMessage(chatId, data.description)
     await bot.sendLocation(chatId, data.latitude, data.longtitude)
   } 
   catch (error) {
