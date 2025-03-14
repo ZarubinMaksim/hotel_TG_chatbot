b90ec5e911d3285894197efe426f9a190309f66b
Maksim Zarubin <89940028777@ya.ru>
2025-03-14 16:38:25 +0700

    updated__managerBot_get-user-info

--- managerBot/managerBot.js
+++ managerBot/managerBot.js
@@ -1,6 +1,7 @@
 
 const User = require('../db/models/user');
 const { createLocalUser } = require('../mainBot/components/currentUsers');
+let keyRequest 
 
 async function setMessageReaction(token, chatId, messageId, emoji) {
   await fetch(`https://api.telegram.org/bot${token}/setMessageReaction`, {
@@ -16,10 +17,19 @@ async function setMessageReaction(token, chatId, messageId, emoji) {
 
 const startManagerBot = (mainBot, managerBot, token) => {
 
+  mainBot.setMyCommands([
+    { command: '/start', description: 'Manage guest' },
+  ]);
+
   managerBot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     managerBot.sendMessage(chatId, 'this is manager bot')
   });
+
+  // managerBot.onText(/\/manage_guest/, (msg) => {
+  //   const chatId = msg.chat.id;
+  //   managerBot.sendMessage(chatId, 'lets manage guest details')
+  // });
 
   managerBot.on('message', async (msg) => {
 
@@ -52,8 +62,29 @@ const startManagerBot = (mainBot, managerBot, token) => {
         await setMessageReaction(token, chatId, msg.reply_to_message.message_id, '👍');
         await mainBot.sendMessage(originalChatId, 'Вы успешно зарегистрировались! Для полного доступа нажмите /start')
         
+    } else if (msg.text === '/manage_guest') {
+      managerBot.sendMessage(chatId, 'Please write room number or guest Last name')
+      keyRequest = 'find_user'
+    } else if (keyRequest === 'find_user') {
+      const userData = msg.text
+      User.find({ 
+        $or: [{ room: userData}, {lastname: userData}] 
+      })
+        .then(result => {
+          console.log('hhfhfhf', result)
+          result.forEach(guestData => {
+            managerBot.sendMessage(chatId, `Here are guests i can found: 
+            Lastname - ${guestData.lastname}
+            Name - ${guestData.name}
+            Room - ${guestData.room}
+            Arrival date - ${guestData.arrival}
+            Departure date - ${guestData.departure}
+           `)
+          })
+
+        })
     } else {
-        managerBot.sendMessage(chatId, 'Error. Are you sure you replying message to confirm registration?');
+      managerBot.sendMessage(chatId, 'Error. Are you sure you replying message to confirm registration?');
     }
   });
 
