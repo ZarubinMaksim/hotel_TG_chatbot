5161eabbf6ba82c3d7c10785b1617a2e6add9996
Maksim Zarubin <89940028777@ya.ru>
2025-03-14 19:20:35 +0700

    update__managerBot_update-guest-info

--- managerBot/managerBot.js
+++ managerBot/managerBot.js
@@ -2,6 +2,7 @@
 const User = require('../db/models/user');
 const { createLocalUser } = require('../mainBot/components/currentUsers');
 let keyRequest 
+let changingUser
 
 async function setMessageReaction(token, chatId, messageId, emoji) {
   await fetch(`https://api.telegram.org/bot${token}/setMessageReaction`, {
@@ -15,11 +16,41 @@ async function setMessageReaction(token, chatId, messageId, emoji) {
   });
 }
 
+
+
 const startManagerBot = (mainBot, managerBot, token) => {
 
   mainBot.setMyCommands([
     { command: '/start', description: 'Manage guest' },
   ]);
+
+  managerBot.on('callback_query', callbackQuery => {
+    const message = callbackQuery.message;
+    const chatId = message.chat.id;
+    const data = callbackQuery.data; 
+    console.log(message, chatId, data)
+  
+    if (data === 'update_guest_details') {
+      managerBot.sendMessage(chatId, message.text, {
+        reply_markup: {
+          inline_keyboard: [
+            [{text: 'Update lastname', callback_data: 'update_guest_lastname'}],
+            [{text: 'Update name', callback_data: 'update_guest_name'}],
+            [{text: 'Update room', callback_data: 'update_guest_room'}],
+            [{text: 'Update arrival', callback_data: 'update_guest_arrival'}],
+            [{text: 'Update departure', callback_data: 'update_guest_departure'}],
+          ]
+        }
+      })
+    } else if (data === 'update_guest_lastname') {
+      keyRequest = data
+      changingUser = message.text
+      console.log(keyRequest)
+    }
+  
+    // Не забудь подтвердить callback, чтобы кнопка перестала "крутиться"
+    managerBot.answerCallbackQuery(callbackQuery.id);
+  });
 
   managerBot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
@@ -79,10 +110,21 @@ const startManagerBot = (mainBot, managerBot, token) => {
             Room - ${guestData.room}
             Arrival date - ${guestData.arrival}
             Departure date - ${guestData.departure}
-           `)
+            Guest ID - ${guestData.chatId}
+           `, {
+            reply_markup: {
+              inline_keyboard: [
+                [{ text: 'Update guest', callback_data: 'update_guest_details'}],
+                [{ text: 'Check out guest', callback_data: 'main_menu'}],
+                [{ text: 'Заполнить форму', switch_inline_query_current_chat: 'Пример текста' }]
+              ]
+            }
+           })
           })
 
         })
+    } else if (keyRequest === 'update_guest_lastname') {
+      console.log('hahahah ya tut', msg.text, changingUser)
     } else {
       managerBot.sendMessage(chatId, 'Error. Are you sure you replying message to confirm registration?');
     }
