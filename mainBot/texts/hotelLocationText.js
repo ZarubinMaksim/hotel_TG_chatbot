e8beb330512b1b72140538f6ac4dac9cf9e95bd9
Maksim Zarubin <89940028777@ya.ru>
2025-05-15 14:13:38 +0700

    update_roomService

--- mainBot/texts/hotelLocationText.js
+++ mainBot/texts/hotelLocationText.js
@@ -1,6 +1,5 @@
 const hotelLocationText = {
-  hotel_address: `Наш адрес: 149 Moo 6 Cherngtalay, Thalang District, Phuket 83110
-  ${<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3580.3343818825106!2d98.29288091008324!3d8.028416591964895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30503997d2597041%3A0xb8253f11d4961ea0!2sLa%20Green%20Hotel%20and%20Residence!5e1!3m2!1sen!2sth!4v1747292951961!5m2!1sen!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>}`,
+  hotel_address: `Наш адрес: 149 Moo 6 Cherngtalay, Thalang District, Phuket 83110`,
   latitude: 8.028443,
   longitude: 98.295394,
 };
