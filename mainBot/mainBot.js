f486a5234349f53eeb7f8b7d28409bc09091af17
Maksim Zarubin <89940028777@ya.ru>
2025-05-16 11:44:17 +0700

    started to test admin panel

--- mainBot/mainBot.js
+++ mainBot/mainBot.js
@@ -16,19 +16,25 @@ const startMainBot = (mainBot, managerBot) => {
     let matched = false;
     sendPromotion(chatId);
     
-    await fetch('https://57a3-103-48-207-179.ngrok-free.app/api/telegram/messages', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        chatId: msg.chat.id,
-        text: msg.text,
-        username: msg.from?.username,
-        firstName: msg.from?.first_name,
-        date: msg.date,
-      }),
-    });
+    try {
+      await fetch('https://57a3-103-48-207-179.ngrok-free.app/api/telegram/messages', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          chatId: msg.chat.id,
+          text: msg.text,
+          username: msg.from?.username,
+          firstName: msg.from?.first_name,
+          date: msg.date,
+        }),
+      });
+    } catch (error) {
+      console.error('❌ Ошибка при отправке сообщения на панель:', error.message);
+    }
+
+    console.log('После fetch: ', msg.text);
 
     for (const { regex, action } of regexHandlers) {
       if (regex.test(text)) {
