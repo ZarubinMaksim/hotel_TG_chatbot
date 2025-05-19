4d0ba5cc7a3b35597c220cae71f05fa1089d70bd
Maksim Zarubin <89940028777@ya.ru>
2025-05-19 16:47:00 +0700

    started to test admin panel

--- mainBot/mainBot.js
+++ mainBot/mainBot.js
@@ -33,7 +33,7 @@ const startMainBot = (mainBot, managerBot) => {
         submitReview(mainBot, managerBot, chatId, msg);
         setKeyRequest(chatId, '');
       } else {
-        console.log('ya tut', keyRequest)
+        console.log('ya tut', servicesDescription[keyRequest], requestDescriptions[keyRequest])
         const replyMsg = replyMap[keyRequest] ?? requestDescriptions.unidentified.userReplyMsg;
         // const messageData = handleManagerBotMessage(msg, guestDetails, keyRequest);
         handleFollowingRequest(mainBot, managerBot, chatId, msg, guestDetails, keyRequest, managerChatId, replyMsg);
